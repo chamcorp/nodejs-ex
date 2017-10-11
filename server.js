28838ccbfada4346f135ff6a4799e0d4dc33c477
chamcorp <31453065+chamcorp@users.noreply.github.com>
2017-10-11 11:22:26 -0400

    Update server.js

--- server.js
+++ server.js
@@ -88,6 +88,9 @@ var initDb = function(callback) {
   });
 };
 
+initDb(function(err){
+  console.log('Error connecting to Mongo. Message:\n'+err);
+});
 app.use('/',session({store:store,secret: 'this-is-a-secret-token', cookie: { maxAge: 600000 }, resave: false, saveUninitialized: true}));
 
 app.get('/', function (req, res) {
@@ -183,9 +186,9 @@ app.use(function(err, req, res, next){
   res.status(500).send('Something bad happened!');
 });
 
-initDb(function(err){
-  console.log('Error connecting to Mongo. Message:\n'+err);
-});
+//initDb(function(err){
+//  console.log('Error connecting to Mongo. Message:\n'+err);
+//});
 
 app.listen(port, ip);
 console.log('Server running on http://%s:%s', ip, port);
